a338d2d845b9e45bf94a0bbd57426e85f061d58e
Andrei Ianikeev <ggg235235@gmail.com>
2023-03-05 13:41:15 +0300

    some

---
+++ leet/1450-Number-of-Students-Doing-Homework-at-a-Given-Time/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int busyStudent(std::vector<int>& startTime, std::vector<int>& endTime, int queryTime) {
+    int res = 0;
+    for(int i = 0 ; i < startTime.size() ; ++i) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            ++res;
+        }
+    }
+    return res;
+}
+
+int main() {
+    
+}
--- leet/933-Number-of-Recent-Calls/code.cpp
+++ leet/933-Number-of-Recent-Calls/code.cpp
@@ -1,7 +1,47 @@
+/*
+6
+ping 1
+ping 1000
+ping 3001
+ping 3002
+ping 3003
+ping 3004
+
+*/
+
 #include <iostream>
-#include <vector>
-#include <string>
+#include <queue>
+
+class RecentCounter {
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        values_.push(t);
+        while(t - values_.front() > 3000) {
+            values_.pop();
+        }
+        return values_.size();
+    }
+
+private:
+    std::queue<int> values_;
+};
 
 int main() {
-    
+    int cmd_num, val;
+    RecentCounter rc;
+    std::cin >> cmd_num;
+    int res;
+    std::string cmd;
+    for(int i = 0 ; i < cmd_num ; ++i) {
+        std::cin >> cmd;
+        if(cmd == "ping") {
+            std::cin >> val;
+            res = rc.ping(val);
+        }
+    }
+    std::cout << res << std::endl;
 }
